65e50d1a0fd79b56c7f89bf779b47207a577e4b0
danju4rizzl <danjumashiwaju@gmail.com>
2020-05-09 17:50:32 +0200

    Created ReusableCard (customwidged)

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,20 +1,78 @@
 import 'package:flutter/material.dart';
 
+final appName = 'bim calculator'.toUpperCase();
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
 class _InputPageState extends State<InputPage> {
+// 0xFF1D1E33
+  // primaryAppColor() => Color(0xFF1D1E33);
+
   @override
   Widget build(BuildContext context) {
-    final appName = 'bim calculator'.toUpperCase();
     return Scaffold(
       appBar: AppBar(
         title: Text(appName),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    cardColor: Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    cardColor: Color(0xFF1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(
+              cardColor: Color(0xFF1D1E33),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    cardColor: Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    cardColor: Color(0xFF1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.cardColor});
+  final Color cardColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: cardColor,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
